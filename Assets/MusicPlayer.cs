4176c9ac26b3f944569e4f0c5bb1aa7049792cea
Jeffrey Earl <jeffreyearl@hotmail.com>
2017-12-23 16:26:13 -0500

    Add terrain and ship

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{	
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadFirstLevel", 2.0f);
+    }
+
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    #region Designer Variables
+
+    [Tooltip("In ms^-1")]
+    [SerializeField]
+    float speed = 20.0f;
+
+    [Tooltip("In meters")]
+    [SerializeField]
+    float xRange = 7.0f;
+
+    [Tooltip("In meters")]
+    [SerializeField]
+    float yRange = 4.75f;
+
+    [SerializeField]
+    float positionPitchFactor = -5.0f;
+
+    [SerializeField]
+    float controlPitchFactor = -20.0f;
+
+    [SerializeField]
+	float positionYawFactor = 5.0f;
+
+	[SerializeField]
+	float controlRollFactor = -20.0f;
+
+    #endregion
+
+    #region Member Variables
+
+    private float xThrow;
+    private float yThrow;
+
+    #endregion
+
+	#region Unity Hooks
+
+    // Update is called once per frame
+    void Update()
+    {
+        CaptureInput();
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+	#endregion
+
+    #region Private Methods
+
+    private void CaptureInput()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+    }
+
+    private void ProcessTranslation()
+    {
+        // Calculate X-Axis movement
+        float xOffsetThisFrame = Time.deltaTime * xThrow * speed;
+        float newXPos = Mathf.Clamp(transform.localPosition.x + xOffsetThisFrame, -xRange, xRange);
+
+        // Calculate Y-Axis movement
+        float yOffsetThisFrame = Time.deltaTime * yThrow * speed;
+        float newYPos = Mathf.Clamp(transform.localPosition.y + yOffsetThisFrame, -yRange, yRange);
+
+        // Set new local position
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+		// Process pitch
+		float pitchFromPosition = transform.localPosition.y * positionPitchFactor;
+		float pitchFromThrow = yThrow * controlPitchFactor;
+        float pitch = pitchFromPosition + pitchFromThrow;
+
+		// Process yaw
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+		// Process roll
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    #endregion
+}
